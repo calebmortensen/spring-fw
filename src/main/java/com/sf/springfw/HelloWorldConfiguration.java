55a021ecb43dcbbf028c613cf46547c2723d7b06
calebmortensen <caleb.mortensen@live.com>
2023-11-30 10:55:27 -0800

    Bean parameters and configuration
    Create new beans and multiple ways to access

--- src/main/java/com/sf/springfw/HelloWorldConfiguration.java
+++ src/main/java/com/sf/springfw/HelloWorldConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //record takes care of verbosity of getter, setter, accessor methods, constructor, equals, hashcode
-record Person  (String name, int age) {};
+record Person  (String name, int age, Address address) {};
 record Address  (String firstLine, String city) {};
 @Configuration
 public class HelloWorldConfiguration {
@@ -21,13 +21,31 @@ public class HelloWorldConfiguration {
 	
 	@Bean
 	public Person person() {
-		return new Person("James", 20);
+		return new Person("James", 20, new Address ("Main St", "Charleston"));
 		
 	}
 	
 	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address());
+		
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) { //using parameters, not calling methods directly
+		return new Person(name, age, address3);
+		
+	}
+	
+	@Bean(name = "address2")
 	public Address address() {
 		return new Address("12345 Road Dr.", "San Diego");
+		
+	}
+	
+	@Bean(name = "address3")
+	public Address address3() {
+		return new Address("6789 Nowhere dr", "Seattle");
 		
 	}
 }
