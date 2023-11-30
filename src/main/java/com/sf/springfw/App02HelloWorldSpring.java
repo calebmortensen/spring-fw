788f2a2bf9debb0aee766d87985252bf5b7bf3c2
calebmortensen <caleb.mortensen@live.com>
2023-11-30 13:48:05 -0800

    Primary and Qualifier
    Spring Bean Annotations

--- src/main/java/com/sf/springfw/App02HelloWorldSpring.java
+++ src/main/java/com/sf/springfw/App02HelloWorldSpring.java
@@ -1,5 +1,7 @@
 package com.sf.springfw;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App02HelloWorldSpring {
@@ -20,7 +22,14 @@ public class App02HelloWorldSpring {
 		System.out.println(context.getBean("person2MethodCall"));
 		System.out.println(context.getBean("person3Parameters"));
 		System.out.println(context.getBean("address2"));
-		//System.out.println(context.getBean(Address.class)); //entire record / same as above
+		System.out.println(context.getBean(Address.class)); //entire record / same as above
+		System.out.println(context.getBean(Religion.class)); //This works because only 1 method call to record
+		//Functional Programming
+		/*
+		 * Arrays.stream(context.getBeanDefinitionNames())
+		 * .forEach(System.out::println);
+		 */
+		
 
 	}
 
--- src/main/java/com/sf/springfw/HelloWorldConfiguration.java
+++ src/main/java/com/sf/springfw/HelloWorldConfiguration.java
@@ -1,11 +1,14 @@
 package com.sf.springfw;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 //record takes care of verbosity of getter, setter, accessor methods, constructor, equals, hashcode
 record Person  (String name, int age, Address address) {};
 record Address  (String firstLine, String city) {};
+record Religion (String religion) {};
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -19,7 +22,15 @@ public class HelloWorldConfiguration {
 		return 44;
 	}
 	
+
 	@Bean
+	public Religion religion() {
+		return new Religion("atheist");
+		
+	}
+	
+	@Bean
+	@Primary
 	public Person person() {
 		return new Person("James", 20, new Address ("Main St", "Charleston"));
 		
@@ -32,18 +43,27 @@ public class HelloWorldConfiguration {
 	}
 	
 	@Bean
-	public Person person3Parameters(String name, int age, Address address3) { //using parameters, not calling methods directly
-		return new Person(name, age, address3);
+	public Person person3Parameters(String name, int age, @Qualifier("address3qualifier") Address address) { //using parameters, not calling methods directly
+		return new Person("Using QUALIFIER " + name, age, address);
 		
 	}
 	
 	@Bean(name = "address2")
+	@Primary   //If more than one Address is called in Address.class, this will fix error
 	public Address address() {
 		return new Address("12345 Road Dr.", "San Diego");
 		
 	}
 	
+	@Bean(name = "address4")
+	@Qualifier("address4qualifier")
+	public Address address4() {
+		return new Address("666 Bart Dr.", "San Antonio");
+		
+	}
+	
 	@Bean(name = "address3")
+	@Qualifier("address3qualifier")
 	public Address address3() {
 		return new Address("6789 Nowhere dr", "Seattle");
 		
